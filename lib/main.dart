dd4a8b2bfae403f38b71d712eb1d5077d6587944
FrazileDevelopers <frazile.com@gmail.com>
2019-01-26 19:41:08 +0530

    orientation fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:ytapp/home.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(YTAPP());
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  runApp(YTAPP());
+}
 
 class YTAPP extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: true,
-    theme: ThemeData(primaryColor: Colors.white, accentColor: Colors.black),
-    title: "DAMDAMI TAKSAL",
-    home: YTHome(),
-  );
+        debugShowCheckedModeBanner: true,
+        theme: ThemeData(primaryColor: Colors.white, accentColor: Colors.black),
+        title: "DAMDAMI TAKSAL",
+        home: YTHome(),
+      );
 }
