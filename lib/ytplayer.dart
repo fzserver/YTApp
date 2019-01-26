6bf0b170be2ba4183018f92605cb85a7b7e469be
Suraj Tiwari <surajtiwari020@gmail.com>
2019-01-26 19:22:59 +0530

    dynamic video orientation

--- lib/ytplayer.dart
+++ lib/ytplayer.dart
@@ -5,7 +5,9 @@ class YTPlayer extends StatefulWidget {
   String youtubeapi;
   String videoID;
   String title;
+
   YTPlayer({this.youtubeapi, this.videoID, this.title});
+
   @override
   _YTPlayerState createState() => _YTPlayerState();
 }
@@ -16,55 +18,67 @@ class _YTPlayerState extends State<YTPlayer> {
     super.dispose();
   }
 
+  Center _ytPlayer() {
+    return Center(
+      child: YoutubePlayer(
+        source: widget.videoID,
+        quality: YoutubeQuality.HD,
+        showThumbnail: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // backgroundColor: Color(0xfff8faf8),
-        centerTitle: true,
-        elevation: 1.0,
-        title: Text(
-          'DAMDAMI TAKSAL',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.deepOrange,
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Center(
-              child: YoutubePlayer(
-                source: widget.videoID,
-                quality: YoutubeQuality.HD,
-                showThumbnail: true,
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        if (orientation == Orientation.portrait) {
+          return Scaffold(
+            appBar: AppBar(
+              // backgroundColor: Color(0xfff8faf8),
+              centerTitle: true,
+              elevation: 1.0,
+              title: Text(
+                'DAMDAMI TAKSAL',
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
+              backgroundColor: Colors.deepOrange,
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Container(
-                height: 80.0,
-                width: double.infinity,
-                child: Card(
-                  elevation: 2.0,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Center(
-                      child: Text(
-                        '${widget.title}',
-                        style: TextStyle(
-                            color: Colors.deepOrange,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16.0),
+            body: Container(
+              child: Column(
+                children: <Widget>[
+                  _ytPlayer(),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Container(
+                      height: 80.0,
+                      width: double.infinity,
+                      child: Card(
+                        elevation: 2.0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Center(
+                            child: Text(
+                              '${widget.title}',
+                              style: TextStyle(
+                                  color: Colors.deepOrange,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16.0),
+                            ),
+                          ),
+                        ),
+                        color: Colors.yellow,
                       ),
                     ),
                   ),
-                  color: Colors.yellow,
-                ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
+          );
+        } else {
+          return _ytPlayer();
+        }
+      },
     );
   }
 }
